53e1c7fabc0aa4b32100c243a97d12ca62a0a52a
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-27 19:23:11 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  attr_reader :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.all
+    @name
+  end
+
+end
